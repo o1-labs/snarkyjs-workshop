2f1f6b6aaa72b23914e20ec9fc1d416bab9a2c4d
Izaak Meckler <ihmeckler@gmail.coom>
2021-12-06 09:37:19 -0800

    bonus

---
+++ src/bonus.ts
@@ -0,0 +1,147 @@
+import {
+  Field,
+  prop,
+  PublicKey,
+  CircuitValue,
+  Signature,
+  UInt64,
+  UInt32,
+  KeyedAccumulatorFactory,
+  ProofWithInput,
+  proofSystem,
+  branch,
+  MerkleStack,
+} from '@o1labs/snarkyjs';
+
+const AccountDbDepth: number = 32;
+const AccountDb = KeyedAccumulatorFactory<PublicKey, RollupAccount>(
+  AccountDbDepth
+);
+type AccountDb = InstanceType<typeof AccountDb>;
+
+class RollupAccount extends CircuitValue {
+  @prop balance: UInt64;
+  @prop nonce: UInt32;
+  @prop publicKey: PublicKey;
+
+  constructor(balance: UInt64, nonce: UInt32, publicKey: PublicKey) {
+    super();
+    this.balance = balance;
+    this.nonce = nonce;
+    this.publicKey = publicKey;
+  }
+}
+
+class RollupTransaction extends CircuitValue {
+  @prop amount: UInt64;
+  @prop nonce: UInt32;
+  @prop sender: PublicKey;
+  @prop receiver: PublicKey;
+
+  constructor(
+    amount: UInt64,
+    nonce: UInt32,
+    sender: PublicKey,
+    receiver: PublicKey
+  ) {
+    super();
+    this.amount = amount;
+    this.nonce = nonce;
+    this.sender = sender;
+    this.receiver = receiver;
+  }
+}
+
+class RollupDeposit extends CircuitValue {
+  @prop publicKey: PublicKey;
+  @prop amount: UInt64;
+  constructor(publicKey: PublicKey, amount: UInt64) {
+    super();
+    this.publicKey = publicKey;
+    this.amount = amount;
+  }
+}
+
+class RollupState extends CircuitValue {
+  @prop pendingDepositsCommitment: Field;
+  @prop accountDbCommitment: Field;
+  constructor(p: Field, c: Field) {
+    super();
+    this.pendingDepositsCommitment = p;
+    this.accountDbCommitment = c;
+  }
+}
+
+class RollupStateTransition extends CircuitValue {
+  @prop source: RollupState;
+  @prop target: RollupState;
+  constructor(source: RollupState, target: RollupState) {
+    super();
+    this.source = source;
+    this.target = target;
+  }
+}
+
+// a recursive proof system is kind of like an "enum"
+@proofSystem
+class RollupProof extends ProofWithInput<RollupStateTransition> {
+  @branch static processDeposit(
+    pending: MerkleStack<RollupDeposit>,
+    accountDb: AccountDb
+  ): RollupProof {
+    let before = new RollupState(pending.commitment, accountDb.commitment());
+    let deposit = pending.pop();
+    let [{ isSome }, mem] = accountDb.get(deposit.publicKey);
+    isSome.assertEquals(false);
+
+    let account = new RollupAccount(
+      UInt64.zero,
+      UInt32.zero,
+      deposit.publicKey
+    );
+    accountDb.set(mem, account);
+
+    let after = new RollupState(pending.commitment, accountDb.commitment());
+
+    return new RollupProof(new RollupStateTransition(before, after));
+  }
+
+  @branch static transaction(
+    t: RollupTransaction,
+    s: Signature,
+    pending: MerkleStack<RollupDeposit>,
+    accountDb: AccountDb
+  ): RollupProof {
+    s.verify(t.sender, t.toFields()).assertEquals(true);
+    let stateBefore = new RollupState(
+      pending.commitment,
+      accountDb.commitment()
+    );
+
+    let [senderAccount, senderPos] = accountDb.get(t.sender);
+    senderAccount.isSome.assertEquals(true);
+    senderAccount.value.nonce.assertEquals(t.nonce);
+
+    senderAccount.value.balance = senderAccount.value.balance.sub(t.amount);
+    senderAccount.value.nonce = senderAccount.value.nonce.add(1);
+
+    accountDb.set(senderPos, senderAccount.value);
+
+    let [receiverAccount, receiverPos] = accountDb.get(t.receiver);
+    receiverAccount.value.balance = receiverAccount.value.balance.add(t.amount);
+    accountDb.set(receiverPos, receiverAccount.value);
+
+    let stateAfter = new RollupState(
+      pending.commitment,
+      accountDb.commitment()
+    );
+    return new RollupProof(new RollupStateTransition(stateBefore, stateAfter));
+  }
+
+  @branch static merge(p1: RollupProof, p2: RollupProof): RollupProof {
+    p1.publicInput.target.assertEquals(p2.publicInput.source);
+    return new RollupProof(
+      new RollupStateTransition(p1.publicInput.source, p2.publicInput.target)
+    );
+  }
+}
